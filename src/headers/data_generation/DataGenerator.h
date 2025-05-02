0feb265bb5a78e7b9bd0f30a4986c815f3e33b1b
perepilka <yaroslavperepilka@gmail.com>
2025-05-02 10:35:46 +0200

    general commit

--- src/headers/data_generation/DataGenerator.h
+++ src/headers/data_generation/DataGenerator.h
@@ -7,11 +7,70 @@
 
 #include <vector>
 #include "Types.h"
+#include <algorithm>
+#include <random>
 
 class DataGenerator {
   public:
     template<typename Type>
-    static std::vector<Type>  generate(size_t size, ArrayTypes array_type);
+    static std::vector<Type>  generate(size_t size, ArrayTypes array_type) {
+        std::vector<Type> vec;
+        vec.reserve(size);
+
+        std::random_device rd;
+        std::mt19937 gen(rd());
+
+
+        // type = FLOAT
+        if constexpr (std::is_floating_point_v<Type>) {
+            std::uniform_real_distribution<Type> dist(0.0f, 10000.0f);
+            for(size_t i = 0; i < size; ++i){
+                vec.push_back(dist(gen));
+            }
+        }
+        // type = INT
+        if constexpr (std::is_integral_v<Type> && !std::is_same_v<DataType, bool>){
+            std::uniform_int_distribution<Type> dist(0, 10000);
+            for(size_t i = 0; i < size; ++i){
+                vec.push_back(dist(gen));
+            }
+        }
+        //type = CHAR
+        if constexpr (std::is_same_v<DataType, char>) {
+            std::uniform_int_distribution<int> dist('a', 'z');
+            for(size_t i = 0; i < size; ++i){
+                vec.push_back(static_cast<char>(dist(gen)));
+            }
+        }
+
+        switch(array_type){
+            case ArrayTypes::RANDOM:{
+                break;
+            }
+            case ArrayTypes::SORTED:{
+                std::sort(vec.begin(), vec.end());
+                break;
+            }
+            case ArrayTypes::REVERSE_SORTED:{
+                std::sort(vec.rbegin(), vec.rend());
+                break;
+            }
+            case ArrayTypes::PARTIALLY_SORTED_33:{
+                if (size >= 3) {
+                    std::sort(vec.begin(), vec.begin() + size/3);
+                }
+                break;
+            }
+            case ArrayTypes::PARTIALLY_SORTED_66:{
+                if (size >= 3) {
+                    std::sort(vec.begin(), vec.begin() + 2*size/3);
+                }
+                break;
+            }
+        }
+
+        return vec;
+    }
     template<typename Type>
     static void save(std::vector<Type> & vec);
     template<typename Type>
--- src/implementations/main.cpp
+++ src/implementations/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <../headers/data_generation/DataGenerator.h>
 
-
 DataGenerator generator;
 std::vector<int> int_vec;
 std::vector<float> float_vec;
@@ -21,78 +20,77 @@ int main() {
             size_t size;
             std::cout << "Wybierz ilosc elementow do sortowania: ";
             std::cin >> size;
+
             int type;
-            datatype_choosing:
+        datatype_choosing:
             std::cout << "Wybierz typ dannych do generacji: \n"
-                    "1. int\n2. float\n3. char\n ";
+                         "1. int\n2. float\n3. char\n";
             std::cin >> type;
-            arraytype_choosing:
-            std::cout << "Wybiers typ tablicy do generowania: \n"
-                    "1. Elementy ulozone losowo\n"
-                    "2. Elementy posortowano rosnaco\n"
-                    "3. Elementy posortowano malejaco\n"
-                    "4. 33% procenty elemetow posortowano rosnaco\n"
-                    "5. 33% procenty elemetow posortowano rosnaco\n";
 
-            std::cin >> x;
+            int array_choice;
+        arraytype_choosing:
+            std::cout << "Wybierz typ tablicy do generowania: \n"
+                         "1. Elementy ulozone losowo\n"
+                         "2. Elementy posortowano rosnaco\n"
+                         "3. Elementy posortowano malejaco\n"
+                         "4. 33% elementow posortowano rosnaco\n"
+                         "5. 66% elementow posortowano rosnaco\n";
+
+            std::cin >> array_choice;
             ArrayTypes array_type;
-            switch(x){
-                    {
-                    case 1: {
-                        array_type = ArrayTypes::RANDOM;
-                        break;
-                    }
-                    case 2: {
-                        array_type = ArrayTypes::SORTED;
-                        break;
-                    }
-                    case 3: {
-                        array_type = ArrayTypes::REVERSE_SORTED;
-                        break;
-                    }
-                    case 4: {
-                        array_type = ArrayTypes::PARTIALLY_SORTED_33;
-                        break;
-                    }
-                    case 5: {
-                        array_type = ArrayTypes::PARTIALLY_SORTED_66;
-                        break;
-                    }
-                    default: {
-                        std::cout << "Wybrana zly typ tablicy, wpisz powtornie: "
-                        goto arraytype_choosing;
-                        break;}
-                    }
 
-                    std::cout << "Generacja...";
-                    switch (type) {
-                        //1. int
-                        case 1: {
-                            int_vec = DataGenerator::generate<int>(size, array_type);
-                        }
-                        //2. float
-                        case 2: {
-                            float_vec = DataGenerator::generate<float>(size, array_type);
-                        }
-                        //3. char
-                        case 3: {
-                            char_vec = DataGenerator::generate<char>(size, array_type);
-                        }
-                        default: {
-                            std::cout << "Wybrany zly typ danych, wpisz powtornie: "
-                            goto datatype_choosing;
-                            break;
-                        }
-                    }
+            switch (array_choice) {
+                case 1:
+                    array_type = ArrayTypes::RANDOM;
+                    break;
+                case 2:
+                    array_type = ArrayTypes::SORTED;
+                    break;
+                case 3:
+                    array_type = ArrayTypes::REVERSE_SORTED;
+                    break;
+                case 4:
+                    array_type = ArrayTypes::PARTIALLY_SORTED_33;
+                    break;
+                case 5:
+                    array_type = ArrayTypes::PARTIALLY_SORTED_66;
+                    break;
+                default:
+                    std::cout << "Wybrano zly typ tablicy, wpisz powtornie.\n";
+                    goto arraytype_choosing;
             }
 
+            std::cout << "Generacja...\n";
 
+            switch (type) {
+                case 1:
+                    int_vec = DataGenerator::generate<int>(size, array_type);
+                    break;
+                case 2:
+                    float_vec = DataGenerator::generate<float>(size, array_type);
+                    break;
+                case 3:
+                    char_vec = DataGenerator::generate<char>(size, array_type);
+                    break;
+                default:
+                    std::cout << "Wybrano zly typ danych, wpisz powtornie.\n";
+                    goto datatype_choosing;
+            }
 
+            break;
+        }
 
-            //2. Wczytywanie z pliku
         case 2: {
+            // Tutaj możesz dodać kod do wczytywania z pliku
+            std::cout << "Funkcja wczytywania z pliku nie jest jeszcze zaimplementowana.\n";
+            break;
+        }
 
+        default: {
+            std::cout << "Niepoprawna opcja. Zakonczono program.\n";
+            break;
         }
-        }
-        //1. Generacja nowych dannych
     }
+
+    return 0;
+}
