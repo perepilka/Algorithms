28cc86b0a16a3f7d90a16efd554ecd0979492229
perepilka <yaroslavperepilka@gmail.com>
2025-05-04 16:04:22 +0200

    timer added commit

--- src/headers/utils/Helper.h
+++ src/headers/utils/Helper.h
@@ -9,23 +9,28 @@
 #include <filesystem>
 
 
+class Helper {
+    std::chrono::high_resolution_clock::time_point start_time;
 
-class Helper{
+public:
+    template<typename Type>
+    static void print_vec(std::vector<Type> &vec) {
+        for (auto i: vec) {
+            std::cout << i << " ";
+        }
+        std::cout << std::endl;
+    }
 
-  public:
-    template <typename Type>
-    static void print_vec(std::vector<Type> &vec){
-      for (auto i : vec){
-        std::cout << i << " ";
-      }
-      std::cout << std::endl;
+     void timer_start() {
+        start_time = std::chrono::high_resolution_clock::now();
+    }
+     double timer_stop() {
+        auto end = std::chrono::high_resolution_clock::now();
+        return std::chrono::duration<double, std::milli>(end - start_time).count();
     }
 
 
-
-
 };
-
 
 
 
